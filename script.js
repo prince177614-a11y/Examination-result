06b324a71cab0806d252cfbc15e0c9ee105db0ef
Prince Gupta <prince177614@gmail.com>
2026-05-03 22:08:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// Quetion 1. -  Examination Result
+// 90+ -> A+
+// 80+ -> A
+// 70+ -> B
+// 60+ -> C
+// 50+ -> D
+// 40+ -> Fail
+
+
+
+let marks = prompt("Enter your Examination marks");
+
+if(marks > 100)
+{
+    console.log("MARKS JYADA NHI AAYEGA JITNA PADH LO")
+}    
+else if(marks > 90)
+{
+    console.log("A+")
+}    
+else if(marks > 80)
+{
+    console.log("A")
+}    
+else if(marks > 70)
+{
+    console.log("B")
+}    
+else if(marks > 60)
+{
+    console.log("C")
+} 
+else if(marks > 50)
+{
+    console.log("D")
+} 
+else if(marks > 40)
+{
+    console.log("Fail")
+}          
